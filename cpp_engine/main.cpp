6d6f87c61bb55d73d71d5a05b49f5385f5700d16
itsme_mobin <soleimanimobin15@gmail.com>
2026-02-22 22:43:30 +0330

    last commit

--- cpp_engine/main.cpp
+++ cpp_engine/main.cpp
@@ -26,41 +26,100 @@ float dif_calcul(const vector <float> &A,const vector <float> &B){
         return vec_dot / (sqrt(vec_a_power) * sqrt(vec_b_power));
 }
 int main(){
+ 
+	ifstream file("vector.bin", ios::binary);
+        //read query bin file
 
-	//read bin file 
-	
-	ifstream file(
-        "mini_vector_search_engine/vector.bin",
-        ios::binary
-    );
-	if (!file){
-
-		cerr << "CRITICAL ERROR: Could not find or open the file!" << endl;
-		return 1;
-	}
-
-	int num_vector {0};
-	int dim {0};
-
-	file.read(reinterpret_cast<char*>(&num_vector),sizeof(int));
-	file.read(reinterpret_cast<char*>(&dim),sizeof(int));
+        ifstream file_q("query.bin",ios::binary);
 
 
-	vector <float> buffer(dim) ;
-       	int count =0 ; 
+        if (!file && !file_q){
 
-	while(file.read(reinterpret_cast<char*>(buffer.data()),dim * sizeof(float))){
-	
-		cout <<"read_vector"<<count<<"first element"<<buffer[0];
 
-		count++;
+                cerr << "CRITICAL ERROR: Could not find or open the file!" << endl;
 
-		
-	
-	}
+                return 1;
 
-	file.close();
+        }
 
-	cout<< "read file succes";	
+
+        vector <float> qvec(384,0.0f);
+
+	file_q.read(reinterpret_cast<char*>(qvec.data()), qvec.size() * sizeof(float));
+
+
+
+        // read vector or database file
+
+
+        float max_score = 0.0;
+
+        int num_vector {0};
+
+        int dim {0};
+
+
+        file.read(reinterpret_cast<char*>(&num_vector),sizeof(int));
+
+        file.read(reinterpret_cast<char*>(&dim),sizeof(int));
+
+
+
+        vector <float> buffer(dim) ;
+
+        int count =0 ;
+
+
+        int best_index = -1;
+
+        while(file.read(reinterpret_cast<char*>(buffer.data()),dim * sizeof(float))){
+
+
+                float cal = dif_calcul(qvec,buffer );
+
+
+                if (cal > max_score ){
+
+
+                        max_score = cal ;
+
+                        best_index = count;
+
+                }
+
+
+                count++;
+
+
+
+
+        }
+
+
+        file.close();
+
+
+
+        string result ;
+
+
+        ifstream text_file("vector.txt"); 
+
+	if (text_file.is_open() && best_index != -1){
+    		for(int j = 0 ; j <= best_index ; j++ ){
+    		    getline(text_file, result);
+    		}
+   
+    	cout << "Best Match: " << result << endl; 
+    	cout << "Confidence Score: " << max_score << endl;
+    	text_file.close();
+	} else {
+
+        	cout << "No match found or file error." << endl;
+
+        }
+
+ 
+ 
 
 }
--- python_script/main.py
+++ python_script/main.py
@@ -16,24 +16,30 @@ data=input("add your input text: ")
 
 embedding = embedid_engine(data)
 
-file = 'vector.bin'
-
-dim = embedding.shape[0]
-num = 1
-file_exists = os.path.isfile(file)
-with open(file, "ab") as f:
-    if not file_exists:
-        f.write(struct.pack("ii", 1, dim))
-    f.write(embedding.tobytes())
-with open("vector.txt",mode = 'a',encoding="utf-8") as vector:
-    vector.write(data + "\n")
-    vector.close()
-
-
 user_input = input("add your input query: ")
 
 embedding_query = embedid_engine(user_input)
 
-with open("/home/hello-world/mini_vector_search_engine/cpp_engine/query_test.bin",mode = 'wb') as query:
+file = 'mini_vector_search_engine/cpp_engine/vector.bin'
+
+dim = embedding.shape[0]
+num = 1
+file_exists = os.path.isfile(file)
+if not os.path.isfile(file):
+    with open(file, "wb") as f:
+        f.write(struct.pack("ii", 1, dim))
+        f.write(embedding.tobytes())
+else:
+    with open(file, "r+b") as f:
+        current_num = struct.unpack("i", f.read(4))[0]
+        new_num = current_num + 1
+        f.seek(0)
+        f.write(struct.pack("i", new_num))
+        f.seek(0, os.SEEK_END)
+        f.write(embedding.tobytes())
+with open("mini_vector_search_engine/cpp_engine/vector.txt", mode='a', encoding="utf-8") as vector:
+    vector.write(data + "\n")
+
+with open("mini_vector_search_engine/cpp_engine/query.bin",mode = 'wb') as query:
     query.write(embedding_query.tobytes())
     query.close()
