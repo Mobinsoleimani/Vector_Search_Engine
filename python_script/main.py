058744d21a2d85801d9c0b7a058f1f12f4316b9c
mobin <110524624+Mobinsoleimani@users.noreply.github.com>
2026-02-22 22:46:35 +0330

    Update main.py

--- python_script/main.py
+++ python_script/main.py
@@ -20,7 +20,7 @@ user_input = input("add your input query: ")
 
 embedding_query = embedid_engine(user_input)
 
-file = 'mini_vector_search_engine/cpp_engine/vector.bin'
+file = 'vector_search_engine/cpp_engine/vector.bin'
 
 dim = embedding.shape[0]
 num = 1
@@ -37,9 +37,9 @@ else:
         f.write(struct.pack("i", new_num))
         f.seek(0, os.SEEK_END)
         f.write(embedding.tobytes())
-with open("mini_vector_search_engine/cpp_engine/vector.txt", mode='a', encoding="utf-8") as vector:
+with open("vector_search_engine/cpp_engine/vector.txt", mode='a', encoding="utf-8") as vector:
     vector.write(data + "\n")
 
-with open("mini_vector_search_engine/cpp_engine/query.bin",mode = 'wb') as query:
+with open("vector_search_engine/cpp_engine/query.bin",mode = 'wb') as query:
     query.write(embedding_query.tobytes())
     query.close()
